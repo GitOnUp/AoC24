730249a7d88deb9f2c86dfa2c732311a1c543247
George Murray <murray.george.m@gmail.com>
2024-12-01 14:38:37 -0800

    p1part2

--- p1.py
+++ p1.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 
 INPUT_LINE_MATCHER = re.compile(r"(\d+)\s+(\d+)")
 
@@ -11,17 +12,30 @@ def get_inputs():
         l, r = parsed.groups(0)
         l_arr.append(int(l))
         r_arr.append(int(r))
-    return l_arr, r_arr
+    return sorted(l_arr), sorted(r_arr)
+
+
+l_array, r_array = get_inputs()
 
 
 def run():
-    l_array, r_array = get_inputs()
     tot_d = 0
-    for l, r in zip(sorted(l_array), sorted(r_array)):
+    for l, r in zip(l_array, r_array):
         d = abs(l - r)
         tot_d += d
     return tot_d
 
 
+def run2():
+    freq = defaultdict(int)
+    score = 0
+    for i in r_array:
+        freq[i] += 1
+    for j in l_array:
+        score += j * freq.get(j, 0)
+    return score
+
+
 if __name__ == "__main__":
     print(run())
+    print(run2())
