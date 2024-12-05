da81453f4c37f27a59423d9ea39960fdb8bd0e60
George Murray <murray.george.m@gmail.com>
2024-12-05 10:59:48 -0800

    Day 5 part 2

--- p5.py
+++ p5.py
@@ -7,6 +7,7 @@ def parse():
         in_rules = True
 
         middles_total = 0
+        new_middles_total = 0
         for line in f.readlines():
             line = line.strip()
             if in_rules:
@@ -34,12 +35,37 @@ def parse():
                         return False
                 return True
 
+            def fix_by_rules():
+                relevant_rules = set()
+                for page in indexes:
+                    for rule in rules.get(page, []):
+                        earlier, later = rule
+                        if earlier in indexes and later in indexes:
+                            relevant_rules.add(rule)
+                new_update = []
+                while len(new_update) < len(update):
+                    candidates = set(indexes.keys()) - set(new_update)
+                    if len(candidates) == 1:
+                        new_update.append(candidates.pop())
+                        break
+                    for rule in relevant_rules:
+                        earlier, later = rule
+                        candidates.discard(later)
+                        if len(candidates) == 1:
+                            added_item = candidates.pop()
+                            new_update.append(added_item)
+                            relevant_rules = set(filter(lambda r: r[0] != added_item, relevant_rules))
+                            break
+                new_middle = new_update[len(new_update) // 2]
+                return new_middle
+
             for page in update:
                 if not valid_by_rules(page):
+                    new_middles_total += fix_by_rules()
                     break
             else:
                 middles_total += middle
-        return middles_total
+        return middles_total, new_middles_total
 
 
 if __name__ == "__main__":
