22839dc4a128d6f78cf4afbf9575035814c81de5
George Murray <murray.george.m@gmail.com>
2024-12-16 16:23:24 -0800

    Day 9 p2

--- p9.py
+++ p9.py
@@ -1,4 +1,20 @@
 
+class FileDef:
+    id: int
+    start: int
+    end: int
+    size: int
+
+    def __init__(self, id: int, start: int, end: int):
+        self.id = id
+        self.start = start
+        self.end = end
+        self.size = end - start
+
+    def __repr__(self):
+        return f"{self.id}: {self.start}-{self.end} ({self.size})"
+
+
 def pack(blocks):
     ix = 0
     reversed_ix_ix = 0
@@ -26,11 +42,15 @@ if __name__ == "__main__":
     with open("p9.input.txt") as f:
         ftab = f.readline().strip()
     blocks = {}
+    files = []  # (ix, len), ix into files is file ID
+    gaps = []
     current_index = 0
     current_id = 0
     for i, c in enumerate(ftab):
         if i % 2 == 0:
-            for ix in range(int(c)):
+            file_length = int(c)
+            files.append(FileDef(current_id, current_index, current_index + file_length))
+            for ix in range(file_length):
                 blocks[current_index] = current_id
                 current_index += 1
             current_id += 1
@@ -39,3 +59,23 @@ if __name__ == "__main__":
 
     pack(blocks)
     print(checksum(blocks))
+
+    iback = len(files) - 1
+    while iback >= 0:
+        last_file = files[iback]
+        for i in range(len(files[:iback])):
+            gap = files[i+1].start - files[i].end
+            if gap >= last_file.size:
+                files = files[:i+1] + [last_file] + files[i+1:iback] + files[iback+1:]
+                last_file.start = files[i].end
+                last_file.end = last_file.start + last_file.size
+                break
+        else:
+            iback -= 1
+
+    files_checksum = 0
+    for file in files:
+        for ix in range(file.start, file.end):
+            files_checksum += ix * file.id
+
+    print(files_checksum)
