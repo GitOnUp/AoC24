6844b4f4568ec5fe3a30b1ffa3c39a0ed356312f
George Murray <murray.george.m@gmail.com>
2024-12-16 16:40:15 -0800

    Day 10 p2

--- p10.py
+++ p10.py
@@ -16,24 +16,29 @@ def at(topography, x, y):
 
 def trailhead_reachable(topography, height_start, x, y):
     if at(topography, x, y) != height_start:
-        return set()
+        return set(), 0
     if height_start == 9:
-        return {(x, y)}
+        return {(x, y)}, 1
 
     height_start += 1
     rval = set()
-    rval |= trailhead_reachable(topography, height_start, x+1, y)
-    rval |= trailhead_reachable(topography, height_start, x, y+1)
-    rval |= trailhead_reachable(topography, height_start, x-1, y)
-    rval |= trailhead_reachable(topography, height_start, x, y-1)
-    return rval
+    rating = 0
+    for newx, newy in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]:
+        reached, subrating = trailhead_reachable(topography, height_start, newx, newy)
+        rval |= reached
+        rating += subrating
+    return rval, rating
 
 
 if __name__ == "__main__":
     topography = read_input()
     score = 0
+    rating = 0
     for y in range(len(topography)):
         for x in range(len(topography[y])):
-            score += len(trailhead_reachable(topography, 0, x, y))
+            reachable, subrating = trailhead_reachable(topography, 0, x, y)
+            score += len(reachable)
+            rating += subrating
 
     print(score)
+    print(rating)
