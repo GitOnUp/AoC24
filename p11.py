c869c19216c6bd628071f28973ee9a8d1f9f2ed1
George Murray <murray.george.m@gmail.com>
2024-12-19 09:04:56 -0800

    Blink multiple stones at once

--- p11.py
+++ p11.py
@@ -2,52 +2,36 @@ from collections import defaultdict, deque
 
 
 def read_input():
-    with open("p11.test.input.txt") as f:
+    with open("p11.input.txt") as f:
         return [int(s) for s in f.readline().strip().split(" ")]
-
-
-def build_cache(nblinks):
-    cache = defaultdict(list)
-    for i in range(100):
-        stones = [i]
-        cache[i].append(len(stones))
-        for ixblink in range(1, nblinks + 1):
-            stones = do_blink(stones)
-            cache[i].append(len(stones))
-    return cache
-
-
-def do_blinks(stones, nblinks):
-    ds = deque(stones)
-    total = 0
-    while len(stones):
-        working_stones = [(stones[0], 0)]
-        for i in range(nblinks):
-            if stones[0] in cache:
-                total += cache[stones[0]][nblinks - i]
-            stones = do_blink(stones)
-    return stones
 
 
 def do_blink(stones):
     new_stones = []
     for stone in stones:
-        if stone == 0:
-            new_stones.append(1)
+        val, mult = stone
+        if val == 0:
+            new_stones.append((1, mult))
         else:
-            dstr = str(stone)
+            dstr = str(val)
             l = len(dstr)
             if l % 2 == 0:
-                new_stones.append(int(dstr[:l//2]))
-                new_stones.append(int(dstr[l//2:]))
+                new_stones.append((int(dstr[:l//2]), mult))
+                new_stones.append((int(dstr[l//2:]), mult))
             else:
-                new_stones.append(stone * 2024)
-
-    return new_stones
+                new_stones.append((val * 2024, mult))
+    counts = defaultdict(int)
+    for stone in new_stones:
+        counts[stone[0]] += stone[1]
+    return list(counts.items())
 
 
 if __name__ == "__main__":
-    stones = read_input()
-    cache = build_cache(25)
-    stones = do_blinks([(stone, 0) for stone in stones], 25)
-    print(len(stones))
+    stones = [(stone, 1) for stone in read_input()]
+    for i in range(75):
+        stones = do_blink(stones)
+    total = 0
+    for stone in stones:
+        _, mult = stone
+        total += mult
+    print(total)
