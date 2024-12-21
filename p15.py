d6e1c0cdfd1b3fa683e87f6f8592c86e0c116276
George Murray <murray.george.m@gmail.com>
2024-12-20 23:53:10 -0800

    Day 15 p2

--- p15.py
+++ p15.py
@@ -21,7 +21,7 @@ def read_input():
     grid = []
     moves = []
     doing_grid = True
-    with open("p15.test.input.txt", "r") as f:
+    with open("p15.input.txt", "r") as f:
         for line in f.readlines():
             line = line.strip()
             if not line:
@@ -48,25 +48,39 @@ def read_input():
 def attempt_move(grid, robot_coord, move):
     dx, dy = move
     x, y = robot_coord
-    stack = [(ROBOT, x + dx, y + dy)]
-    x, y = x + dx, y + dy
+    stack = [[(ROBOT, x + dx, y + dy, x, y)]]  # Item, new x, new y, prev x, prev y
     while True:
-        item = grid[y][x]
-        if item == WALL:
-            return robot_coord
-        if item == BOX:
-            stack.append((BOX, x + dx, y + dy))
-        if item == EMPTY:
+        topstack = stack[-1]
+        new_items = []
+        for movement in topstack:
+            _, x, y, _, _ = movement
+            item = grid[y][x]
+            if item == WALL:
+                return robot_coord
+            if item == BOX:
+                new_items.append((BOX, x + dx, y + dy, x, y))
+            if item in [BOX_L, BOX_R]:
+                new_items.append((item, x + dx, y + dy, x, y))
+                if move in [DOWN, UP]:
+                    if item == BOX_L:
+                        new_items.append((BOX_R, x + 1 + dx, y + dy, x + 1, y))
+                    else:
+                        new_items.append((BOX_L, x - 1 + dx, y + dy, x - 1, y))
+            if item == EMPTY:
+                continue
+        if len(new_items) == 0:
             break
-        x, y = x + dx, y + dy
+        stack.append(new_items)
 
     new_robot_coord = None
     while len(stack) > 0:
-        item, x, y = stack.pop()
-        grid[y][x] = item
-        if item == ROBOT:
-            new_robot_coord = (x, y)
-    grid[robot_coord[1]][robot_coord[0]] = EMPTY
+        movements = stack.pop()
+        for movement in movements:
+            item, x, y, prevx, prevy = movement
+            grid[y][x] = item
+            grid[prevy][prevx] = EMPTY
+            if item == ROBOT:
+                new_robot_coord = (x, y)
     return new_robot_coord
 
 
@@ -93,16 +107,8 @@ def sum_gps(grid):
     total = 0
     for y in range(len(grid)):
         for x in range(len(grid[y])):
-            if grid[y][x] == BOX:
+            if grid[y][x] in (BOX, BOX_L):
                 total += (y * 100) + x
-    return total
-
-
-def sum_gps_wide(grid):
-    total = 0
-    for y in range(len(grid)):
-        for x in range(len(grid[y])):
-            pass
     return total
 
 
@@ -116,4 +122,4 @@ if __name__ == "__main__":
     grid, robot_coord = expand_grid(grid)
     for move in moves:
         robot_coord = attempt_move(grid, robot_coord, move)
-    print(sum_gps_wide(grid))
+    print(sum_gps(grid))
