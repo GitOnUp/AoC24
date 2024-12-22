345889e93c0e09f2cab45e66c0f91cec2a8ee84f
George Murray <murray.george.m@gmail.com>
2024-12-22 12:26:04 -0800

    Day 17 p2

--- p17.py
+++ p17.py
@@ -68,28 +68,52 @@ def process_one(registers, program, ip) -> (int, Optional[int]):
         assert False
 
 
-if __name__ == "__main__":
-    registers, program = read_input()
+def run(registers, program):
+    working_registers = registers[:]
     ip = 0
     output_arr = []
-    working_registers = registers[:]
     while ip < len(program):
         ip, output = process_one(working_registers, program, ip)
         if output is not None:
             output_arr.append(output)
+    return output_arr
+
+
+def print_output(output_arr):
     print(",".join([str(i) for i in output_arr]))
 
-"""
-Program:
 
-2,4: B = A % 8
-1,1: B = B ^ 1
-7,5: C = A / B
-1,5: B = B ^ 5
-4,2: B = B ^ C
-5,5: output.append(B % 8)
-0,3: A = A // (2**3)
-3,0: If A > 0 loop to 2,4
+if __name__ == "__main__":
+    registers, program = read_input()
+    output_arr = run(registers, program)
+    print_output(output_arr)
+    """
+    Program:
 
-- Start with last output and feed value of A to make it hit that, and work backwards
-"""
+    2,4: B = A % 8
+    1,1: B = B ^ 1
+    7,5: C = A // 2**B
+    1,5: B = B ^ 5
+    4,2: B = B ^ C
+    5,5: output.append(B % 8)
+    0,3: A = A // (2**3)
+    3,0: If A > 0 loop to 2,4
+
+    - This appears to be looking at each octet in sequence from least sig to most
+    - Start with trying to match -1st program octet and then add more sig octets to A
+    """
+
+    def search(A, i):
+        for a in range(0, 8):
+            A_tmp = A + a
+            output_arr = run([A_tmp, 0, 0], program)
+            if output_arr == program:
+                return A_tmp
+            if output_arr == program[-len(output_arr):]:
+                r = search(A_tmp * 8, i+1)
+                if r:
+                    return r
+        return None
+
+    A = 0
+    print(search(A, 0))
