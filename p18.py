499c2f0689af1754d16ec07d2076777ccc9cedbd
George Murray <murray.george.m@gmail.com>
2024-12-24 22:56:20 -0800

    Day 18 p1 and notes

---
+++ p18.py
@@ -0,0 +1,55 @@
+from collections import deque
+from dataclasses import dataclass
+
+DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+
+def read_input():
+    corruptions = []
+    fname = "p18.input.txt"
+    dims = (70,70)
+    if "test" in fname:
+        dims = (6, 6)
+    with open(fname, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            x, y = [int(i) for i in line.split(',')]
+            corruptions.append((x, y))
+    return corruptions, dims
+
+
+if __name__ == "__main__":
+    corruptions, dims = read_input()
+
+    cset = set(corruptions[:1024])
+    seen = set()
+
+    @dataclass
+    class State:
+        x: int
+        y: int
+        path: set
+
+        def __lt__(self, other):
+            return self.y < other.y and self.x < other.x
+
+        def pos(self):
+            return (self.x, self.y)
+
+    search = deque([State(0, 0, set())])
+    while len(search) > 0:
+        head = search.popleft()
+        if head.pos() == dims:
+            print(len(head.path))
+            break
+        for d in DIRECTIONS:
+            dx, dy = d
+            nx, ny = head.x + dx, head.y + dy
+            if nx < 0 or ny < 0 or nx > dims[0] or ny > dims[1]:
+                continue
+            if (nx, ny) in seen:
+                continue
+            if (nx, ny) in cset:
+                continue
+            seen.add((nx, ny))
+            search.append(State(nx, ny, head.path | {(nx, ny)}))
