068ad6068f2f7d590b53394ddf328b327c8062b8
George Murray <murray.george.m@gmail.com>
2025-01-04 11:45:43 -0500

    Day 23 p2

--- p23.py
+++ p23.py
@@ -1,4 +1,3 @@
-import re
 from collections import deque, defaultdict
 
 
@@ -9,9 +8,9 @@ def read_input():
             yield link[0], link[1]
 
 
-def build_graph():
+def build_graph(links: [(str, str)]):
     graph = defaultdict(set)
-    for link in read_input():
+    for link in links:
         l1, l2 = link
         graph[l1].add(l2)
         graph[l2].add(l1)
@@ -33,10 +32,37 @@ def find_cycles(graph: {str: {str}}):
     return seen_cycles
 
 
+def find_next_networks(prev_networks: [set], graph: {str: {str}}):
+    next_networks = []
+    seen = set()
+    for network in prev_networks:
+        for next_k in graph:
+            if next_k in network:
+                continue
+            if all([network_node in graph[next_k] for network_node in network]):
+                next_network = network | {next_k}
+                hashkey = ",".join(sorted(next_network))
+                if hashkey in seen:
+                    continue
+                seen.add(hashkey)
+                next_networks.append(network | {next_k})
+    return next_networks
+
+
 if __name__ == "__main__":
     total = 0
-    for cycle in find_cycles(build_graph()):
-        if "t" in (cycle[0], cycle[3], cycle[6]):
-            total += 1
+    links = list(read_input())
+    g = build_graph(links)
+    networks = [{l1, l2} for l1, l2 in links]
+    threes = find_next_networks(networks, g)
+
+    for network in threes:
+        for node in network:
+            if node[0] == "t":
+                total += 1
+                break
     print(total)
+    while len(networks) > 1:
+        networks = find_next_networks(networks, g)
+    print(",".join(sorted(networks[0])))
 
