41000bc7a84293b5ba7f955008491835a8dba847
George Murray <murray.george.m@gmail.com>
2025-01-04 13:52:50 -0500

    Day 24 p1 and testing

---
+++ p24.py
@@ -0,0 +1,54 @@
+from collections import namedtuple
+
+Calc = namedtuple("Calc", ["wires", "op"])
+
+
+def read_input():
+    circuit = {}
+    with open("p24.input.txt", "r") as f:
+        doing_wires = True
+        for line in f.readlines():
+            line = line.strip()
+            if not line:
+                doing_wires = False
+                continue
+            if doing_wires:
+                wire, val = line.split(": ")
+                val = int(val)
+                circuit[wire] = val
+                continue
+            wire1, op, wire2, _, outwire = line.split(" ")
+            circuit[outwire] = Calc([wire1, wire2], op)
+    return circuit
+
+
+def find_value(circuit, key):
+    val = circuit[key]
+    if isinstance(val, int):
+        return val
+    lval = find_value(circuit, val.wires[0])
+    rval = find_value(circuit, val.wires[1])
+    if val.op == "AND":
+        return lval & rval
+    elif val.op == "XOR":
+        return lval ^ rval
+    else:
+        return lval | rval
+
+
+def get_wire_nums(circuit, prefix: str) -> int:
+    total = 0
+    for i, k in enumerate(sorted(filter(lambda key: key[0] == prefix, circuit.keys()))):
+        total += find_value(circuit, k) * 2**i
+    return total
+
+
+if __name__ == "__main__":
+    circuit = read_input()
+    x = get_wire_nums(circuit, "x")
+    y = get_wire_nums(circuit, "y")
+    print(x+y)
+    value = 0
+    for i, k in enumerate(sorted(filter(lambda key: key[0] == "z", circuit.keys()))):
+        value += find_value(circuit, k) * (2**i)
+    print(value)
